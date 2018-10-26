6a07347a518431e9b03d34ef8b875ddf3b4da4dc
Joel Cuyos <jcuyos@cyrious.com>
2018-10-26 13:07:27 +0800

    Test

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossOrigin="anonymous"></link>
         <Router>
           <Switch>
-            <Route path="/message" component={Messaging} />
+            <Route path="/messages" component={Messaging} />
             <Route path="*" component={Messages} />
           </Switch>
         </Router>
